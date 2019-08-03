047da1fac2bf14daab22f552c33e3ba6db9f4fc6
SumOn <sumon.v0.0@gmail.com>
2019-08-03 16:53:10 +0600

    Initial commit

---
+++ app/src/main/java/com/sumon/imageeditor/tools/ToolType.java
@@ -0,0 +1,13 @@
+package com.sumon.imageeditor.tools;
+
+/**
+ * Created by SumOn on 8/3/2019.
+ */
+public enum ToolType {
+    BRUSH,
+    TEXT,
+    ERASER,
+    FILTER,
+    EMOJI,
+    STICKER
+}
